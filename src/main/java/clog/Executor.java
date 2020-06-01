cb32f0a76db09e485d2d304f4d1f6f5ee74ea053
Griko <namba19960409@gmail.com>
2020-06-01 15:46:44 +0900

    exitで処理終了

---
+++ src/main/java/clog/Executor.java
@@ -0,0 +1,44 @@
+package clog;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.Executors;
+
+import lombok.val;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Executor {
+	public static int second = 0;
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader  br = new BufferedReader(isr);
+
+		val ex = Executors.newFixedThreadPool(5);
+
+		ex.submit(()-> {
+			int second = 0;
+			for(;;) {
+				second++;
+				Thread.sleep(1 * 1000);
+				if(second % 10 == 0) {
+					log.info(second + "秒経過");
+				}
+			}
+		});
+		String str = "";
+		for(;;) {
+			str = br.readLine();
+			System.out.println(str);
+			if(str.equals("exit")) {
+				System.out.println("終了しました。");
+				br.close();
+				ex.shutdownNow();
+				break;
+			}
+		}
+	}
+
+}
